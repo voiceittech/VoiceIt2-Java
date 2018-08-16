d83c0c562b7e771127c9047bda87d06153afdba1
gilgameshskytrooper <me@gilgameshskytrooper.io>
2018-08-16 11:41:13 -0500

    this commit shouldnt trigger a new release

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-version=$(curl -s https://api.github.com/repos/voiceittech/VoiceIt2-Java/releases/latest | grep '"tag_name":' |sed -E 's/.*"([^"]+)".*/\1/' | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
-curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-Java/releases
+committitle=$(git log -1 --pretty=%B | head -n 1)
+if [[ $committitle = *"Release"* ]];
+then
+  version=$(curl -s https://api.github.com/repos/voiceittech/VoiceIt2-Java/releases/latest | grep '"tag_name":' |sed -E 's/.*"([^"]+)".*/\1/' | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
+  curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-Java/releases
+fi
