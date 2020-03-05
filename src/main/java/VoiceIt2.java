cfd9d6e78e8c243dfe9dcd25754ce6b0c145e590
gilgameshskytrooper <me@gilgameshskytrooper.io>
2020-03-05 12:52:17 -0600

    test RELEASEPATCH

--- src/main/java/VoiceIt2.java
+++ src/main/java/VoiceIt2.java
@@ -35,7 +35,7 @@ public class VoiceIt2 {
 	private static String BASE_URL = "https://api.voiceit.io";
 	private String notificationUrl = "";
 	private HttpClient httpClient;
-	public static final String VERSION = "1.4.0";
+	public static final String VERSION = "1.4.1";
 
 	public VoiceIt2(String apiKey, String apiToken){
 			HttpClientBuilder clientBuilder = HttpClientBuilder.create();
